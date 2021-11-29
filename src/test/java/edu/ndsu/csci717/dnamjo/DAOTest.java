c9ae3903485331d7ae835818d7c37b012a0839cb
Alex Milender <alexmilender@gmail.com>
2021-11-29 00:33:34 -0600

    Adding new Tests

--- src/test/java/edu/ndsu/csci717/dnamjo/DAOTest.java
+++ src/test/java/edu/ndsu/csci717/dnamjo/DAOTest.java
@@ -2,7 +2,10 @@ package edu.ndsu.csci717.dnamjo;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
+
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class DAOTest {
 
@@ -43,7 +46,6 @@ public class DAOTest {
 
     @Test
     void loginCheck() {
-        Assertions.assertTrue(false, "login fail");
     }
 
     @Test
@@ -71,6 +73,7 @@ public class DAOTest {
     }
 
     @Test
+    //10
     void getConformTenders() {
     }
 
@@ -96,21 +99,74 @@ public class DAOTest {
 
     @Test
     void getSuccessStories() {
+        int num = 1;
+        String company = "m123";
+        String story = "hai hashfdsnvhnfncpocvhdgjfofjudhdhdhdhdhdhdhdhdhdhdhdhbogog";
+        DTOShareInfo dtoShareInfo = new DTOShareInfo(num, company, story);
+        boolean SuccessStories_flag = false;
+        DAO dataAccess = new DAO();
+        try{
+            SuccessStories_flag = dataAccess.getSuccessStories(dtoShareInfo);
+        } catch (SQLException e) {
+            Assertions.fail("SQL Error", e);
+        }
+        Assertions.assertTrue(SuccessStories_flag,"getSuccessStories Successful");
     }
 
     @Test
     void getAllSuccessStories() {
+        ResultSet SuccessStories;
+        DAO dataAccess = new DAO();
+        try{
+            SuccessStories = dataAccess.getAllSuccessStories();
+            String temp = SuccessStories.toString();
+            System.out.println(temp);
+        } catch (SQLException e) {
+            Assertions.fail("SQL Error", e);
+        }
+        Assertions.assertTrue(false,"getAllSuccessStories Successful");
+
     }
 
     @Test
     void getFileFromRegister() {
+        ArrayList<String> returnedFile;
+        String returnedFileName = "";
+        DAO dataAccess = new DAO();
+        try{
+            returnedFile = dataAccess.getFileFromRegister();
+            returnedFileName = returnedFile.get(0).toString();
+        } catch (SQLException e) {
+            Assertions.fail("SQL Error", e);
+        }
+        Assertions.assertTrue(returnedFileName.contains("Android.docx"), "getFileFromRegister Successful");
     }
 
     @Test
     void getDetails() {
+        String userEmail = "ram@gmail.com";
+        String asdf = "";
+        ResultSet userEmail_returned;
+        DAO dataAccess = new DAO();
+        try{
+            userEmail_returned = dataAccess.getDetails(userEmail);
+        } catch (SQLException e) {
+           Assertions.fail("SQL Error", e);
+        }
+        Assertions.assertTrue(false,"getDetails Successful");
     }
 
     @Test
     void getFileNameUsingregisterId() {
+        String registerId = new String("x123");
+        String registerId_file = new String("noImage");
+        String registerId_retunedFile = "";
+        DAO dataAccess = new DAO();
+        try {
+            registerId_retunedFile = dataAccess.getFileNameUsingregisterId(registerId);
+        } catch (SQLException e) {
+            Assertions.fail("SQL Error", e);
+        }
+        Assertions.assertTrue(registerId_file.equals(registerId_retunedFile), "getFileNameUsingregisterId Successful");
     }
 }
